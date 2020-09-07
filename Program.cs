864d476e6ffd2d7f94781cc4e986012e7bd8ee60
Kirill Sinkevich <sinkevich-kir@mail.ru>
2020-09-08 02:06:39 +0300

    Added vsFileC#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
